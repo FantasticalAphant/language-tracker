041b82e8161c0c5e75e18a50716351a4a880d3cb
David Lin <dlin88@binghamton.edu>
2024-09-16 11:56:23 -0400

    Use tags to separate API endpoints

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,14 @@ from . import crud
 from . import schemas
 from .database import get_db
 
-app = FastAPI()
+tags_metadata = [
+    {
+        "name": "word lists",
+        "description": "List of HSK words",
+    },
+]
+
+app = FastAPI(tags_metadata=tags_metadata)
 
 router = APIRouter()
 
@@ -15,7 +22,7 @@ async def root():
     return {"message": "Hello World"}
 
 
-@router.get("/level/{level}/words", response_model=list[schemas.Word])
+@router.get("/level/{level}/words", response_model=list[schemas.Word], tags=["word_lists"])
 def get_hsk_level_words(level: int, db: Session = Depends(get_db), limit: int = 100, offset: int = 0):
     hsk_level = crud.get_words_by_level(db, level, limit, offset)
     if not hsk_level:
