082851039e5370ec359311e4767a1e2d5dd031d1
David Lin <dlin88@binghamton.edu>
2024-09-26 13:33:17 -0400

    Add dashboard and sentences pages

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,32 @@
+import Navbar from "../components/NavBar.jsx";
+
+export default function HomePage() {
+    return (
+        <>
+            {/*
+        This example requires updating your template:
+
+        ```
+        <html class="h-full">
+        <body class="h-full">
+        ```
+      */}
+            <div className="min-h-full">
+                <Navbar/>
+
+                <div className="py-10">
+                    <header>
+                        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                            <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">Dashboard</h1>
+                        </div>
+                    </header>
+                    <main>
+                        <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
+                            {/* Your content */}
+                        </div>
+                    </main>
+                </div>
+            </div>
+        </>
+    )
+}
