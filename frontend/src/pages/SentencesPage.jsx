fe1b6b386624dfa647dd60ed782880e7e6a30abf
David Lin <dlin88@binghamton.edu>
2024-10-22 16:44:17 -0400

    Print out number of sentences shown

--- frontend/src/pages/SentencesPage.jsx
+++ frontend/src/pages/SentencesPage.jsx
@@ -211,7 +211,8 @@ export default function SentencesPage() {
                 <div className="py-10">
                     <header>
                         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                            <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">Sentences</h1>
+                            <h1 className="text-3xl font-bold leading-tight tracking-tight text-gray-900">Sentences
+                                ({data.length})</h1>
                         </div>
                         <Combobox>
                             <div className="relative">
