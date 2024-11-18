581237d68b4093436f1a91cb0fc707c671169af2
David Lin <dlin88@binghamton.edu>
2024-11-18 12:30:33 -0500

    Create login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter as Router, Navigate, Route, Routes} from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import SentencesPage from "./pages/SentencesPage.jsx";
 import DictionaryPage from "./pages/DictionaryPage.jsx";
@@ -9,8 +9,20 @@ import TranslatorPage from "./pages/TranslatorPage.jsx";
 import WordListsPage from "./pages/WordListsPage.jsx";
 import NotFoundPage from "./pages/NotFoundPage.jsx";
 import SentencePage from "./pages/SentencePage.jsx";
+import LogInPage from "./pages/LogInPage.jsx";
+import {useState} from "react";
+
+// eslint-disable-next-line react/prop-types
+const ProtectedRoute = ({children, isAuthenticated}) => {
+    if (!isAuthenticated) {
+        return <Navigate to={"/login"} replace/>;
+    }
+    return children;
+}
 
 function App() {
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
     return (
         <Router>
             <div>
@@ -19,15 +31,38 @@ function App() {
                     <Routes>
                         {/* Default Route */}
                         <Route path='*' element={<NotFoundPage/>}/>
-                        <Route path="/" element={<HomePage/>}/>
+                        <Route path={"/dashboard"} element={<HomePage/>}/>
+                        <Route path={"/login"} element={<LogInPage/>}/>
                         <Route path={"/dictionary"} element={<DictionaryPage/>}/>
                         <Route path={"/hsk_lists"} element={<HSKListsPage/>}/>
                         <Route path={"/sentences"} element={<SentencesPage/>}/>
                         <Route path={"/sentences/:sentenceId"} element={<SentencePage/>}/>
                         <Route path={"/analyzer"} element={<AnalyzerPage/>}/>
                         <Route path={"/translator"} element={<TranslatorPage/>}/>
-                        <Route path={"/word_lists/:listId"} element={<WordListsPage/>}/>
-                        <Route path={"/word_lists"} element={<WordListsPage/>}/>
+                        <Route
+                            path={"/word_lists/:listId"}
+                            element={
+                                <ProtectedRoute isAuthenticated={isAuthenticated}>
+                                    <WordListsPage/>
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route
+                            path={"/word_lists"}
+                            element={
+                                <ProtectedRoute isAuthenticated={isAuthenticated}>
+                                    <WordListsPage/>
+                                </ProtectedRoute>
+                            }
+                        />
+                        <Route
+                            path={"/"}
+                            element={
+                                isAuthenticated ?
+                                    <Navigate to={"/dashboard"} replace/> :
+                                    <Navigate to={"/login"} replace/>
+                            }
+                        />
                     </Routes>
                 </main>
             </div>
