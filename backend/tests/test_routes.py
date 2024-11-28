217d03132181e0dcfb9b6ede03636a0b46dfecb3
David Lin <dlin88@binghamton.edu>
2024-11-28 15:54:11 -0500

    Use transaction rollbacks for tests

--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -1,9 +1,77 @@
 import pytest
+import sqlalchemy
 from fastapi.testclient import TestClient
+from sqlalchemy.orm import sessionmaker
 
+from backend.app.database import get_db
 from backend.app.main import app
 
-client = TestClient(app)
+SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+
+engine = sqlalchemy.create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+# These two event listeners are only needed for sqlite for proper
+# SAVEPOINT / nested transaction support. Other databases like postgres
+# don't need them.
+# From: https://docs.sqlalchemy.org/en/14/dialects/sqlite.html#serializable-isolation-savepoints-transactional-ddl
+@sqlalchemy.event.listens_for(engine, "connect")
+def do_connect(dbapi_connection, connection_record):
+    # disable pysqlite's emitting of the BEGIN statement entirely.
+    # also stops it from emitting COMMIT before any DDL.
+    dbapi_connection.isolation_level = None
+
+
+@sqlalchemy.event.listens_for(engine, "begin")
+def do_begin(conn):
+    # emit our own BEGIN
+    conn.exec_driver_sql("BEGIN")
+
+
+# This fixture is the main difference to before. It creates a nested
+# transaction, recreates it when the application code calls session.commit
+# and rolls it back at the end.
+# Based on: https://docs.sqlalchemy.org/en/14/orm/session_transaction.html#joining-a-session-into-an-external-transaction-such-as-for-test-suites
+@pytest.fixture()
+def session():
+    connection = engine.connect()
+    transaction = connection.begin()
+    session = TestingSessionLocal(bind=connection)
+
+    # Begin a nested transaction (using SAVEPOINT).
+    nested = connection.begin_nested()
+
+    # If the application code calls session.commit, it will end the nested
+    # transaction. Need to start a new one when that happens.
+    @sqlalchemy.event.listens_for(session, "after_transaction_end")
+    def end_savepoint(session, transaction):
+        nonlocal nested
+        if not nested.is_active:
+            nested = connection.begin_nested()
+
+    yield session
+
+    # Rollback the overall transaction, restoring the state before the test ran.
+    session.close()
+    transaction.rollback()
+    connection.close()
+
+
+# A fixture for the fastapi test client which depends on the
+# previous session fixture. Instead of creating a new session in the
+# dependency override as before, it uses the one provided by the
+# session fixture.
+@pytest.fixture()
+def client(session):
+    def override_get_db():
+        yield session
+
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
+    del app.dependency_overrides[get_db]
 
 
 @pytest.mark.parametrize(
@@ -17,7 +85,7 @@ client = TestClient(app)
         (6, 2500),
     ],
 )
-def test_get_hsk_level_words(level, expected_count):
+def test_get_hsk_level_words(client, level, expected_count):
     """
     Check if the lengths of the HSK lists match up.
     Basically just checking if the HSK lists are actually created.
@@ -39,35 +107,35 @@ def test_get_hsk_level_words(level, expected_count):
         (72501, "你空闲的时候一般做些什么？"),
     ],
 )
-def test_get_sentence(sentence_id, sentence_text):
+def test_get_sentence(client, sentence_id, sentence_text):
     """Check if the sentences are created."""
     response = client.get(f"/sentence/{sentence_id}")
     assert response.status_code == 200
     assert response.json()["text"] == sentence_text
 
 
-def test_get_sentences_default():
+def test_get_sentences_default(client):
     """Check if the number of sentences sent is correct."""
     response = client.get("/sentences")
     assert response.status_code == 200
     assert len(response.json()) == 100
 
 
-def test_get_sentences_limit():
+def test_get_sentences_limit(client):
     """Check if the sentence limit is applied."""
     response = client.get("/sentences", params={"limit": 30000})
     assert response.status_code == 200
     assert len(response.json()) == 30000
 
 
-def test_get_dictionary_entries_default():
+def test_get_dictionary_entries_default(client):
     """Check if the dictionary entries are created."""
     response = client.get("/dictionary")
     assert response.status_code == 200
     assert len(response.json()) == 20
 
 
-def test_get_dictionary_entries_limit():
+def test_get_dictionary_entries_limit(client):
     """Check if the entry limit is applied.."""
     response = client.get("/dictionary", params={"limit": 5000})
     assert response.status_code == 200
@@ -80,7 +148,7 @@ def test_get_dictionary_entries_limit():
         ("这是个句子。", ["这", "是", "个", "句子"]),
     ],
 )
-def test_analyzer_submit_text(initial_text, parsed_text):
+def test_analyzer_submit_text(client, initial_text, parsed_text):
     response = client.post("/analyzer", json={
         "text": initial_text,
     })
@@ -94,7 +162,7 @@ def test_analyzer_submit_text(initial_text, parsed_text):
         ("I wish I didn't have to live a life full of regrets.", "我希望我的人生不必充满遗憾。"),
     ],
 )
-def test_translator_submit_text(initial_text, parsed_text):
+def test_translator_submit_text(client, initial_text, parsed_text):
     response = client.post("/translator", json={
         "text": initial_text
     })
@@ -102,25 +170,25 @@ def test_translator_submit_text(initial_text, parsed_text):
     assert response.json()["text"] == parsed_text
 
 
-def test_create_wordlist():
+def test_create_wordlist(client):
     response = client.post("/wordlists", json={})
 
 
-def test_read_wordlist():
+def test_read_wordlist(client):
     response = client.get("/wordlists/1")
 
 
-def test_read_wordlists():
+def test_read_wordlists(client):
     response = client.get("/wordlists")
 
 
-def test_get_entry_wordlists():
+def test_get_entry_wordlists(client):
     response = client.get("/wordlists/entries/1")
 
 
-def test_update_wordlists():
+def test_update_wordlists(client):
     response = client.post("/wordlists/entries/1", json={})
 
 
-def test_delete_wordlists():
+def test_delete_wordlists(client):
     response = client.delete("/wordlists/1")
