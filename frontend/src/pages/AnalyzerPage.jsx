2dbacff80d2fea35e1391cb63d6f4abd6f0fce03
David Lin <davidlin9812@gmail.com>
2025-01-29 21:05:34 -0500

    Show input text history for analyzer

--- frontend/src/pages/AnalyzerPage.jsx
+++ frontend/src/pages/AnalyzerPage.jsx
@@ -2,10 +2,12 @@ import {useState} from "react";
 import TextArea from "../components/TextArea.jsx";
 import Layout from "../components/Layout.jsx";
 import {API_URL} from "../../utils/api.js";
+import {useLocalStorage} from "react-use";
 
 export default function AnalyzerPage() {
     const [data, setData] = useState([])
     const [text, setText] = useState('');
+    const [history, setHistory] = useLocalStorage("history", []);
 
     const handleSubmit = async (event) => {
         event.preventDefault();
@@ -25,6 +27,10 @@ export default function AnalyzerPage() {
 
             const data = await response.json();
             setData(data)
+
+            const prevHistory = history.slice(-9);
+            setHistory([...prevHistory, text]);
+
             setText("");
         } catch (error) {
             console.error('Error:', error);
@@ -39,8 +45,8 @@ export default function AnalyzerPage() {
                 <TextArea onTextSubmit={handleSubmit} text={text} setText={setText}
                           action={"analyze"}/>
 
-                <div className="mt-10 text-2xl text-gray-700">
-                    <ul className="grid grid-cols-5 gap-5">
+                <div className="mt-10 text-gray-700">
+                    <ul className="grid grid-cols-10 gap-3">
                         {data && data.map(entry => (
                             <li key={entry["word"]} className="border rounded-md text-center">
                                 <div>Word: {entry["word"]}</div>
@@ -49,6 +55,13 @@ export default function AnalyzerPage() {
                             </li>
                         ))}
                     </ul>
+                </div>
+
+                <div className="text-3xl">
+                    History:
+                    {history.map(item => (
+                        JSON.stringify(item)
+                    ))}
                 </div>
             </Layout>
         </>
