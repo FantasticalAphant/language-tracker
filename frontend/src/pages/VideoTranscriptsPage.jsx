ac6a43a6c6e11f7206a81a86eab154d4f6a155bd
David Lin <davidlin9812@gmail.com>
2025-03-03 12:46:47 -0500

    Set a viewport limit for the transcript text

--- frontend/src/pages/VideoTranscriptsPage.jsx
+++ frontend/src/pages/VideoTranscriptsPage.jsx
@@ -32,44 +32,50 @@ export default function VideoTranscriptsPage() {
             <div className="text-sm -mt-8">
                 Get transcripts for videos on YouTube
             </div>
-            <div className="mt-5 grid">
-                <form onSubmit={e => handleSubmit(e)}>
-                    <input
-                        id="text"
-                        name="text"
-                        type="text"
-                        placeholder="YouTube URL"
-                        aria-label="YouTube URL"
-                        value={url}
-                        onChange={(e) => setUrl(e.target.value)}
-                        className="block w-full rounded-md bg-white p-1 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                    />
-                    <button type="submit" className="bg-indigo-500 text-gray-100 p-1 mt-2 rounded-md">
-                        Submit
-                    </button>
-                </form>
-            </div>
 
-            <div className="my-4 justify-self-center">
-                {transcriptInfo["id"] && <iframe
-                    width="560"
-                    height="315"
-                    src={`https://www.youtube.com/embed/${transcriptInfo["id"]}`}
-                    title="YouTube video player"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    allowFullScreen>
-                </iframe>}
-            </div>
+            {!transcriptInfo["id"] &&
+                <div className="mt-5 grid">
+                    <form onSubmit={e => handleSubmit(e)}>
+                        <input
+                            id="text"
+                            name="text"
+                            type="text"
+                            placeholder="YouTube URL"
+                            aria-label="YouTube URL"
+                            value={url}
+                            onChange={(e) => setUrl(e.target.value)}
+                            className="block w-full rounded-md bg-white p-1 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                        />
+                        <button type="submit" className="bg-indigo-500 text-gray-100 p-1 mt-2 rounded-md">
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            }
 
-            <div>
-                <ol className="list-decimal space-y-2">
-                    {transcriptInfo["transcript"] && transcriptInfo["transcript"].map((subtitle, index) => (
-                        <li key={index}>
-                            <p>Text: {subtitle["text"]}</p>
-                            <p>Start: {subtitle["start"]}</p>
-                            <p>Duration: {subtitle["duration"]}</p>
-                        </li>))}
-                </ol>
-            </div>
+            {transcriptInfo["id"] && <div className="grid">
+                <div className="my-5 justify-self-center">
+                    {<iframe
+                        width="560"
+                        height="315"
+                        src={`https://www.youtube.com/embed/${transcriptInfo["id"]}`}
+                        title="YouTube video player"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowFullScreen>
+                    </iframe>}
+                </div>
+
+                <div className="h-96 overflow-y-auto border border-black rounded-md">
+                    <ol className="list-decimal space-y-2 justify-self-center">
+                        {transcriptInfo["transcript"] && transcriptInfo["transcript"].map((subtitle, index) => (
+                            <li key={index}>
+                                <p>Text: {subtitle["text"]}</p>
+                                <p>Start: {subtitle["start"]}</p>
+                                <p>Duration: {subtitle["duration"]}</p>
+                            </li>))}
+                    </ol>
+                </div>
+            </div>}
+
         </Layout>)
 }
