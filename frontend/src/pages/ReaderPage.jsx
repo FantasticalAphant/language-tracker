9ec3dc82b1ec98f51a87b58c36e34e734faab5bc
David Lin <davidlin9812@gmail.com>
2025-03-18 22:18:42 -0400

    Allow user to upload text files

--- frontend/src/pages/ReaderPage.jsx
+++ frontend/src/pages/ReaderPage.jsx
@@ -1,8 +1,35 @@
 import Layout from "../components/Layout.jsx";
+import {useState} from "react";
 
 export default function ReaderPage() {
+    const [text, setText] = useState(null);
+
+    const handleFileChange = async (event) => {
+        const selectedFile = event.target.files[0];
+
+        if (selectedFile.type === 'text/plain' || selectedFile.name.endsWith('.txt')) {
+            try {
+                const content = await selectedFile.text();
+                setText(content);
+            } catch (error) {
+                console.error('Error reading file:', error);
+            }
+        }
+    }
+
     return (
-        <Layout headerName={"Book Reader"} tabName={"Reader"}>
+        <Layout headerName={"File Reader"} tabName={"Reader"}>
+            <div className="mb-3">
+                <input
+                    type="file"
+                    accept="text/plain,.txt"
+                    onChange={handleFileChange}
+                />
+            </div>
+
+            <div>
+                {text}
+            </div>
         </Layout>
     )
 }
